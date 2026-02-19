5e5f4b858b6c23cefa2afdeb6f36339c9327d7de
chengharv-pen <chengharvp@gmail.com>
2026-02-18 22:39:56 -0500

    first load logic + cleanup

--- js/init.js
+++ js/init.js
@@ -1,24 +1,26 @@
 // Arrow Function that is immediately invoked
 (() => {
   // Theme init
-  const savedTheme = localStorage.getItem("preferredTheme");
-  const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const initialTheme = savedTheme || (systemDark ? 'dark' : 'light');
+  let savedTheme = localStorage.getItem("preferredTheme");
+  if (!savedTheme) {
+    savedTheme = "dark";
+    localStorage.setItem("preferredTheme", savedTheme);
+  }
   document.documentElement.setAttribute("data-bs-theme", initialTheme);
   
   // Language init
-  const savedLang = localStorage.getItem("preferredLanguage");
+  let savedLang = localStorage.getItem("preferredLanguage");
+  if (!savedLang) {
+    savedLang = "en";
+    localStorage.setItem("preferredLanguage", savedLang);
+  }
+
   const isEnglishPage = window.location.pathname.includes('index.html') || window.location.pathname.endsWith('/');
   
-  if (savedLang) {
-    // preference is French but on English page, redirect
-    if (savedLang === 'fr' && isEnglishPage) {
-      window.location.replace('index.fr.html');
-    } 
-    // preference is English but on French page, redirect
-    else if (savedLang === 'en' && !isEnglishPage) {
-      window.location.replace('index.html');
-    }
+  if ((savedLang === 'fr' && isEnglishPage)) {         // preference is French but on English page, redirect
+    window.location.replace('index.fr.html');          //
+  } else if ((savedLang === 'en' && !isEnglishPage)) { // preference is English but on French page, redirect
+    window.location.replace('index.html');
   }
 
   // This event listener performs a fade-out whenever the page loads
@@ -26,9 +28,7 @@
     const loader = document.getElementById('page-loader');
     if (loader) {
       loader.classList.add('fade-out');
-      setTimeout(() => {
-        loader.remove();
-      }, 800); 
+      setTimeout(() => loader.remove(), 800); 
     }
   });
 
